687c1d12c01bdff6c92a25bf5b0d7360ca313cae
Liza Kaniewski <lkaniews@lsoc.org>
2016-05-19 15:17:23 -0500

    semi working session a part works

--- controller.rb
+++ controller.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 require_relative 'models/running.rb'
+enable :sessions
 
 get '/' do 
 	erb :index
 end 
 
+
 post '/' do 
-	user = User.new (params[:miles])
-	game = Game.new (user)
-	@answer = game.subtraction 
+	game = Game.new(params[:miles])
+	session[:game] = game
+	@answer = game.subtraction
 	erb :results
 end
 
--- models/running.rb
+++ models/running.rb
@@ -1,18 +1,11 @@
-class User
+class Game
 	attr_accessor :total, :miles
 	def initialize (miles)
 		@total = 2133
 		@miles = miles.to_i
 	end
-end
-
-class Game
-	attr_accessor :total, :miles
-	def initialize (user)
-		@user = user
-	end
 	def subtraction 
-		@answer = @user.total -= @user.miles
+		@answer = @total -= @miles
 	end
 end
 
